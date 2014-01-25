0fa82a55faa29ad8d2e09590a50fbe9baa2aac14
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2014-01-25 18:40:05 +0400

    c# code

---
+++ c#/Ses/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ziyaddin
+ * Date: 22.12.2013
+ * Time: 14:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Ses
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Init";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 41);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(156, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(93, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Listen";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(30, 64);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(121, 51);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "First click to \"Init\" button, then to \"Listen\" button and then speak.";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(180, 124);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Ses";
+			this.Load += new System.EventHandler(this.Form1Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ c#/Ses/Form1.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ziyaddin
+ * Date: 22.12.2013
+ * Time: 14:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.IO.Ports;
+
+namespace Ses
+{
+    public partial class Form1 : Form
+    {
+    	private SerialPort serialPort1 = new SerialPort("COM4", 9600);
+    	
+        public Form1()
+        {
+            InitializeComponent();
+            
+            serialPort1.Open();
+        }
+        
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+        PromptBuilder build = new PromptBuilder();
+        SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
+        
+        void Button1Click(object sender, EventArgs e)
+		{
+            build.ClearContent();
+            build.AppendText(textBox1.Text);
+            synt.Speak(build);
+            synt.Volume = 100;
+            synt.Rate = 2;
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+            Choices list = new Choices();
+            list.Add(new string[] {"on","off"});
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                engine.RequestRecognizerUpdate();
+                engine.LoadGrammar(gr);
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+                engine.SetInputToDefaultAudioDevice();
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+		}
+
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+    /*        else if (e.Result.Text.ToString() == "Exit" || e.Result.Text.ToString() == "Close")
+            {
+                Application.Exit();
+            }
+            */
+            if (e.Result.Text.ToString() == "on")
+            {
+                serialPort1.WriteLine("1");
+                textBox1.Text = "Turned On!";
+            }
+            else if (e.Result.Text.ToString() == "off")
+            {
+                serialPort1.WriteLine("2");
+                textBox1.Text = "Turned Off!";
+            }
+        }
+        
+        void Form1Load(object sender, EventArgs e)
+		{
+			
+		}
+        
+      }
+    }
